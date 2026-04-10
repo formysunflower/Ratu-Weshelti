08d20450165ccf5a6ea061ff4584c46d128671ad
loveyou <easylimirad15@gmail.com>
2026-04-10 23:21:42 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I LOVE U MY QUEEN BEBEE').split('')
+    const titles = ('I LOVE U BEBEE ❤️').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
